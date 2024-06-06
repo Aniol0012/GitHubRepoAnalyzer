3a8ef4d66642415c3c778060fc60d1bd16ff5e9f
Aniol0012 <aniol0012@gmail.com>
2024-06-06 14:17:07 +0200

    feat: add commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+      "type-enum": [
+        2,
+        "always",
+        [
+          "build",
+          "chore",
+          "ci",
+          "docs",
+          "feat",
+          "fix",
+          "perf",
+          "refactor",
+          "revert",
+          "style",
+          "test",
+          "translation",
+          "security",
+          "changeset",
+          "init",
+          "other",
+        ],
+      ],
+      "subject-full-stop": [0, "never"],
+      "subject-case": [0, "never"],
+      "header-max-length": [0, "always", 72],
+    },
+    ignores: [(commit) => commit.startsWith("Merge")],
+  };
