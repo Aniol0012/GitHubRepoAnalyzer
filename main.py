5ae90c514502eba5569530e40d2db6468f839361
Aniol0012 <aniol0012@gmail.com>
2024-06-06 14:12:16 +0200

    fix: plot showing on config change

--- main.py
+++ main.py
@@ -4,61 +4,86 @@ import config
 
 token = config.TOKEN
 repo = config.REPO
-base_url = f'https://api.github.com/repos/{repo}'
+base_url = f"https://api.github.com/repos/{repo}"
+
 
 def get_commits() -> list[dict]:
-    commits_url = f'{base_url}/commits'
-    headers = {'Authorization': f'token {token}'}
+    commits_url = f"{base_url}/commits"
+    headers = {"Authorization": f"token {token}"}
     commits = []
     while commits_url:
         response = requests.get(commits_url, headers=headers)
         commits.extend(response.json())
-        if 'next' in response.links:
-            commits_url = response.links['next']['url']
+        if "next" in response.links:
+            commits_url = response.links["next"]["url"]
         else:
             break
     return commits
+
 
 def analyze_commits(commits: list) -> dict:
     contributors = {}
     for commit in commits:
 
-        if commit.get('author') and commit['author'].get('login'):
-            username = commit['author']['login']
+        if commit.get("author") and commit["author"].get("login"):
+            username = commit["author"]["login"]
             if username not in contributors:
-                contributors[username] = {'commits': 0, 'lines_added': 0}
-            contributors[username]['commits'] += 1
-            contributors[username]['lines_added'] += 10
+                contributors[username] = {"commits": 0, "lines_added": 0}
+            contributors[username]["commits"] += 1
+            contributors[username]["lines_added"] += 10
     return contributors
+
 
 def get_names(contributors: dict, type: str | None) -> list[str]:
     if not type:
         return [name for name in contributors]
     return [f"{name} [{contributors[name][type]}]" for name in contributors]
 
+
+def get_plots_count() -> int:
+    return sum([config.SHOW_COMMITS_PLOT, config.SHOW_LINES_ADDED_PLOT])
+
+
 def plot_data(contributors: dict) -> None:
-    commit_counts = [contrib['commits'] for contrib in contributors.values()]
-    lines_added = [contrib['lines_added'] for contrib in contributors.values()]
-    
-    fig, ax = plt.subplots(2, 1, figsize=(10, 8))
-    
+    commit_counts = [contrib["commits"] for contrib in contributors.values()]
+    lines_added = [contrib["lines_added"] for contrib in contributors.values()]
+
+    plots_count = get_plots_count()
+    fig, ax = plt.subplots(plots_count, 1, figsize=(10, 8))
+
+    if plots_count == 1:
+        ax = [ax]
+
+    current_plot = 0
+
     if config.SHOW_COMMITS_PLOT:
-        ax[0].bar(get_names(contributors, 'commits'), commit_counts, color=config.COMMITS_BAR_COLOR)
-        ax[0].set_title('Number of Commits per Contributor')
-        ax[0].set_ylabel('Number of Commits')
-    
+        ax[current_plot].bar(
+            get_names(contributors, "commits"),
+            commit_counts,
+            color=config.COMMITS_BAR_COLOR,
+        )
+        ax[current_plot].set_title("Number of Commits per Contributor")
+        ax[current_plot].set_ylabel("Number of Commits")
+        current_plot += 1
+
     if config.SHOW_LINES_ADDED_PLOT:
-        ax[1].bar(get_names(contributors, 'lines_added'), lines_added, color=config.LINES_ADDED_BAR_COLOR)
-        ax[1].set_title('Lines Added per Contributor')
-        ax[1].set_ylabel('Number of Lines Added')
-    
+        ax[current_plot].bar(
+            get_names(contributors, "lines_added"),
+            lines_added,
+            color=config.LINES_ADDED_BAR_COLOR,
+        )
+        ax[current_plot].set_title("Lines Added per Contributor")
+        ax[current_plot].set_ylabel("Number of Lines Added")
+
     plt.tight_layout()
     plt.show()
+
 
 def main() -> None:
     commits = get_commits()
     contributors = analyze_commits(commits)
     plot_data(contributors)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
