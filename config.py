6b1c773eb9ca4c2e43ff0cfbc06b21e759916600
Aniol0012 <aniol0012@gmail.com>
2024-06-06 14:19:45 +0200

    style: lint

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-# This is the configuration file for the application
-
 from os import environ
 from dotenv import load_dotenv
 import os
+
+# This is the configuration file for the application
 
 load_dotenv()
 
@@ -12,6 +12,6 @@ REPO = environ.get("REPO")
 SHOW_COMMITS_PLOT = True
 SHOW_LINES_ADDED_PLOT = True
 
-## Styles
+# Styles
 COMMITS_BAR_COLOR = "steelblue"
 LINES_ADDED_BAR_COLOR = "green"
